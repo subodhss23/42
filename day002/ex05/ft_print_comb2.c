9282165cc41a9ce463b5bc632a1d59bcbf437b0d
Subodh Sharma <susharma@e1z1r3p44.42.us.org>
2018-01-11 20:45:06 -0800

    adding stuff for day 02

---
+++ day002/ex05/ft_print_comb2.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb2.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: susharma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/10 22:08:34 by susharma          #+#    #+#             */
+/*   Updated: 2018/01/11 16:06:33 by susharma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft-putchar(char c);
+
+void	ft_print_comb2(void)
+{
+
+	int a;
+	int u;
+	int d;
+
+	a	= 0;
+	while( a++ <= 10000 )
+	{
+		u = a / 100;
+		d = a % 100;
+		if (u < d)
+		{
+			if ( a != 1)
+			{
+				ft_putchar(',');
+				ft_putchar(' ');
+			}
+			ft_putchar(48 + u / 10);
+			ft_putchar(48 + u % 10);
+			ft_putchar(' ');
+			ft_putchar(48 + d / 10);
+			ft_putchar(48 + d % 10);
+		}
+	}
+}
