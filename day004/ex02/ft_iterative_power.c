a14356860d23086947fce656e536c895cd7f91b0
Subodh Sharma <susharma@e1z1r3p1.42.us.org>
2018-01-13 20:52:24 -0800

    commiting day04

---
+++ day004/ex02/ft_iterative_power.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_iterative_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: susharma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/13 20:11:56 by susharma          #+#    #+#             */
+/*   Updated: 2018/01/13 20:13:16 by susharma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_iterative_power(int nb, int power)
+{
+	int	a;
+
+	a = 1;
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	while (power)
+	{
+		a *= nb;
+		power--;
+	}
+	return (a);
+}
---
+++ day004/ex03/ft_recursive_power.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_recursive_power.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: susharma <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/01/13 20:16:34 by susharma          #+#    #+#             */
+/*   Updated: 2018/01/13 20:17:15 by susharma         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int	ft_recursive_power(int nb, int power)
+{
+	if (power < 0)
+		return (0);
+	if (power == 0)
+		return (1);
+	return (nb * (ft_recursive_power(nb, power - 1)));
+
+}
